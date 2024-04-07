60ceffb181fbae9886120f3a7702ef98bfd1f7b4
Francesco Maida <francesco.maida@gmail.com>
2024-04-07 11:01:41 +0200

    Modificato il programma per usare GitPython

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ import os
 import tomllib
 import subprocess
 
+from git import Repo
 import single
 
 # Gets the current path
@@ -25,6 +26,11 @@ config_path = os.path.join(current_path, "config.toml")
 # Creates the path to the index.html file
 homepage_path = os.path.join(public_path, "index.html")
 
+# Creates an object that points to the GIT Repository
+repo = Repo(current_path)
+repo_origin = repo.remotes.origin
+repo_origin.pull()
+
 # Loads the config file with the feed list
 with open(config_path, "rb") as f:
     config = tomllib.load(f)
@@ -36,6 +42,10 @@ for feed in config["feed"]:
 for feed in feeds:
     parser = single.RaiParser(feed["url"], rss_path)
     parser.process(skip_programmi=False, skip_film=True, date_ok=False)
+    rss_file = feed["url"].split("/")[-1]
+    rss_file_path = os.path.join(rss_path, rss_file + ".xml")
+    repo.index.add(rss_file_path)
+    #breakpoint()
 
 # Write a simple HTML page
 with open(homepage_path, "w", encoding="utf8") as f:
@@ -55,7 +65,7 @@ with open(homepage_path, "w", encoding="utf8") as f:
         """)
     for feed in feeds:
         rss_file = feed["url"].split("/")[-1]
-        title = feed.get("title", rss_file)            
+        title = feed.get("title", rss_file)        
         f.write(f'<li><a href="rss/{rss_file}.xml">{title}</a></li>')
     f.write(
         """
@@ -64,9 +74,12 @@ with open(homepage_path, "w", encoding="utf8") as f:
         </body>
         </html>
         """)
-    
+repo.index.add(homepage_path)
+
 # Create a git commit message
 today = datetime.now().strftime("%d-%m-%Y %H:%M")
-subprocess.run(["git", "add", "public/*"], cwd=current_path)
-subprocess.run(["git", "commit", "-m", today], cwd=current_path)
-subprocess.run(["git", "push", "origin", "main"], cwd=current_path)
+repo.index.commit(today)
+repo_origin.push(refspec='main')
+#subprocess.run(["git", "add", "public/*"], cwd=current_path)
+#subprocess.run(["git", "commit", "-m", today], cwd=current_path)
+#subprocess.run(["git", "push", "origin", "main"], cwd=current_path)
